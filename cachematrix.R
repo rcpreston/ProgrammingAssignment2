5a281667cc81e497795c75f9735bd3e57c93ce31
rcpreston <rianna.preston@gmail.com>
2014-06-17 11:16:30 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions use the cache to store data on a matrix and its inverse.
 
-## Write a short comment describing this function
+## This function makes a matrix that can be put in a cache. To call the
+## function and get a cached matrix, you want to set it to a value:
+## y<-makeCacheMatrix()
+## This will make a variable y that is a list of 4 functions
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+  ix <- NULL
+  set <- function(y) #Sets a cached matrix, storing it in the cache
+  {
+    x <<- y
+    ix <<- NULL
+  }
+  get <- function() x  ## retrieves and outputs the cached matrix
+  
+  setinverse <- function(solve) ix <<- solve ## Sets and inverse matrix to be stored
+  
+  getinverse <- function() ix ## Retrieves and outputs the inverse matrix
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) ## Outputting the list of functions
 }
 
 
-## Write a short comment describing this function
+## Gets the inverse if it is stored, if it is not stored then it sets and outputs
+## an inverse. If the matrix stored has no inverse then the solve function will fail
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...)
+{
         ## Return a matrix that is the inverse of 'x'
+  ix <- x $getinverse() ## Retrieving a cached inverse
+  if(!is.null(ix)) ## Checking if there is actually an inverse
+  {
+    message("Have a cached copy")
+    return(ix) ## End the function and spit out the inverse
+  }
+  data <- x$get() ## Pull the original matrix
+  ix <- solve(data, ...) ## Create an inverse of the original matrix
+  x$setinverse(ix) ## Put that inverse into the cache
+  ix ## Output the inverse
 }
